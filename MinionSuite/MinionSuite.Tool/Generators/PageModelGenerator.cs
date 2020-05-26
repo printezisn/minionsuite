1d7c4b2f7d86a057bc011a1e77e35484b87ac50d
Nikos <printezisn@gmail.com>
2020-05-26 10:46:10 +0300

    Add interface for page model

--- MinionSuite/MinionSuite.Tool/Generators/PageModelGenerator.cs
+++ MinionSuite/MinionSuite.Tool/Generators/PageModelGenerator.cs
@@ -16,10 +16,44 @@ namespace MinionSuite.Tool.Generators
         /// <param name="argReader">Information fetched from the command line arguments</param>
         public void Generate(ArgReader argReader)
         {
+            string interfaceContent = GetInterfaceContent(argReader);
+            FileHelper.SaveToOutput(argReader.OutputFolder, "IPageModel.cs", interfaceContent);
+
+            string classContent = GetClassContent(argReader);
+            FileHelper.SaveToOutput(argReader.OutputFolder, "PageModel.cs", classContent);
+        }
+
+        /// <summary>
+        /// Displays a help message
+        /// </summary>
+        public void ShowHelpMessage()
+        {
             var builder = new StringBuilder();
 
             builder
+                .AppendLine("Usage: minionsuite pagemodel [parameters]")
+                .AppendLine()
+                .AppendLine("Generates a model to handle paging for queryables.")
+                .AppendLine()
+                .AppendLine("Parameters:")
+                .AppendLine("  -ns|--namespace <name>:\tThe namespace of the generated class.")
+                .AppendLine("  -o|--output <path>:\tThe path to the output folder (default: .).");
+
+            Console.WriteLine(builder.ToString());
+        }
+
+        /// <summary>
+        /// Returns the content of the class implementation
+        /// </summary>
+        /// <param name="argReader">Information fetched from the command line arguments</param>
+        /// <returns>The content of the class implementation</returns>
+        private string GetClassContent(ArgReader argReader)
+        {
+            var builder = new StringBuilder();
+
+            return builder
                 .AppendNestedLine(0, "using System;")
+                .AppendNestedLine(0, "using System.Collections;")
                 .AppendNestedLine(0, "using System.Collections.Generic;")
                 .AppendNestedLine(0, "using System.Linq;")
                 .AppendNestedLine(0, "using System.Threading.Tasks;")
@@ -27,7 +61,7 @@ namespace MinionSuite.Tool.Generators
                 .AppendLine()
                 .AppendNestedLine(0, $"namespace {argReader.Namespace}")
                 .AppendNestedLine(0, "{")
-                .AppendNestedLine(1, "public class PageModel<T>")
+                .AppendNestedLine(1, "public class PageModel<T> : IPageModel, IEnumerable<T>")
                 .AppendNestedLine(1, "{")
                 .AppendNestedLine(2, "public IEnumerable<T> Collection { get; private set; }")
                 .AppendNestedLine(2, "public int TotalItems { get; private set; }")
@@ -51,33 +85,50 @@ namespace MinionSuite.Tool.Generators
                 .AppendNestedLine(3, "pageModel.TotalPages = (int)Math.Ceiling(pageModel.TotalItems / (double)pageModel.PageSize);")
                 .AppendNestedLine(3, "pageModel.Page = Math.Max(1, page);")
                 .AppendNestedLine(3, "pageModel.Page = Math.Min(pageModel.Page, pageModel.TotalPages);")
-                .AppendNestedLine(3, " pageModel.Collection = await query.Skip((pageModel.Page - 1) * pageModel.PageSize).Take(pageModel.PageSize).ToListAsync();")
+                .AppendNestedLine(3, "pageModel.Collection = await query.Skip((pageModel.Page - 1) * pageModel.PageSize).Take(pageModel.PageSize).ToListAsync();")
                 .AppendLine()
                 .AppendNestedLine(3, "return pageModel;")
                 .AppendNestedLine(2, "}")
+                .AppendLine()
+                .AppendNestedLine(2, "public IEnumerator<T> GetEnumerator()")
+                .AppendNestedLine(2, "{")
+                .AppendNestedLine(3, "return Collection.GetEnumerator();")
+                .AppendNestedLine(2, "}")
+                .AppendLine()
+                .AppendNestedLine(2, "IEnumerator IEnumerable.GetEnumerator()")
+                .AppendNestedLine(2, "{")
+                .AppendNestedLine(3, "return Collection.GetEnumerator();")
+                .AppendNestedLine(2, "}")
                 .AppendNestedLine(1, "}")
-                .AppendNestedLine(0, "}");
-
-            FileHelper.SaveToOutput(argReader.OutputFolder, "PageModel.cs", builder.ToString());
+                .AppendNestedLine(0, "}")
+                .ToString();
         }
 
         /// <summary>
-        /// Displays a help message
+        /// Returns the content of the interface
         /// </summary>
-        public void ShowHelpMessage()
+        /// <param name="argReader">Information fetched from the command line arguments</param>
+        /// <returns>The content of the interface</returns>
+        private string GetInterfaceContent(ArgReader argReader)
         {
             var builder = new StringBuilder();
 
-            builder
-                .AppendLine("Usage: minionsuite pagemodel [parameters]")
+            return builder
+                .AppendNestedLine(0, "using System;")
                 .AppendLine()
-                .AppendLine("Generates a model to handle paging for queryables.")
-                .AppendLine()
-                .AppendLine("Parameters:")
-                .AppendLine("  -ns|--namespace <name>:\tThe namespace of the generated class.")
-                .AppendLine("  -o|--output <path>:\tThe path to the output folder (default: .).");
-
-            Console.WriteLine(builder.ToString());
+                .AppendNestedLine(0, $"namespace {argReader.Namespace}")
+                .AppendNestedLine(0, "{")
+                .AppendNestedLine(1, "public interface IPageModel")
+                .AppendNestedLine(1, "{")
+                .AppendNestedLine(2, "int TotalItems { get; }")
+                .AppendNestedLine(2, "int TotalPages { get; }")
+                .AppendNestedLine(2, "int Page { get; }")
+                .AppendNestedLine(2, "int PageSize { get; }")
+                .AppendNestedLine(2, "bool IsFirstPage { get; }")
+                .AppendNestedLine(2, "bool IsLastPage { get; }")
+                .AppendNestedLine(1, "}")
+                .AppendNestedLine(0, "}")
+                .ToString();
         }
     }
 }
